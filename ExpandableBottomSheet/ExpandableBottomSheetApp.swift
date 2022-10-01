04b1c53e729397f269bae123bc9fab7414287e7d
Ramil <ram4ik@hotmail.com>
2022-10-01 13:39:33 +0300

    Initial Commit

---
+++ ExpandableBottomSheet/ExpandableBottomSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpandableBottomSheetApp.swift
+//  ExpandableBottomSheet
+//
+//  Created by Ramill Ibragimov on 01.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct ExpandableBottomSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
